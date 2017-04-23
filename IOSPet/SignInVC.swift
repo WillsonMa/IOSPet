2bd2a67dba9c5abb792404a3197eb6fc90c6f7d8
ZIQI MA <willsonma@ZIQIs-MacBook-Pro.local>
2017-04-23 18:19:19 -0400

    Completed setting up Firebase integration

---
+++ IOSPet/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOSPet
+//
+//  Created by ZIQI MA on 4/23/17.
+//  Copyright © 2017 ZIQI MA. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
